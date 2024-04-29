3e3e92ae6495e75f15d024501b1a8f5ad19b652a
Chernika_56 <bosko.working@mail.ru>
2024-04-29 17:21:40 +0300

    add UriGenerator

---
+++ lab2/UriGenerator/GeneratorUri.cs
@@ -0,0 +1,21 @@
+﻿using FakerLab.Generators.StringGenerators;
+
+namespace FakerLab.Generators.UrlGenerator
+{
+    public class GeneratorUri : IGenerator<Uri>
+    {
+        public Uri GetValue()
+        {
+            var generator = new GeneratorString();
+
+            var builder = new UriBuilder
+            {
+                Scheme = "http",
+                Host = "localhost",
+                Path = generator.GetValue()
+            };
+
+            return builder.Uri;
+        }
+    }
+}
